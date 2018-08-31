9fbc5209e834122889d2d8f4a9fb53ef211179db
Tahseen Kazi <myronintro@gmail.com>
2018-08-31 03:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
-  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
