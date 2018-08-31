d26e14bef37ec9ee7f6904ccb36738138170747c
Tahseen Kazi <myronintro@gmail.com>
2018-08-31 02:41:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object[key];
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
