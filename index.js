75e21d914a73d879e3092aceac39742d7a39d268
Tahseen Kazi <myronintro@gmail.com>
2018-08-31 02:43:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, {key : value});
-  return object[key];
+  var newObj = Object.assign({}, object, {key : value});
+  return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
