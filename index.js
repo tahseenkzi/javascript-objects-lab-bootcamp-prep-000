8fb0acdc344aeea192f6d99c87b8fc328cb2330f
Tahseen Kazi <myronintro@gmail.com>
2018-08-31 03:03:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function 
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object, key);
+  delete newObj.key;
+  return newObj;
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
